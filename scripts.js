ae4642f9862e972687a436f1532f5612f0be43c9
GabrielNarcizo <eligcan@hotmail.com>
2024-10-15 13:55:40 -0300

    feat: funcionalidade de adicionar tarefa

--- scripts.js
+++ scripts.js
@@ -1,14 +1,28 @@
-var todos = [
-    {
-        text: 'oi',
-        done: true
-    }
-]
+var todos = [{
+    text: 'oi',
+    done: true
+}]
 
 const todosApp = {
-    data(){
+    data() {
         return {
-            todos: window.todos
+            todos: window.todos,
+            newTodo: {
+                done: false
+            }
+        }
+    },
+    methods: {
+        addTodo: function () {
+            if (this.newTodo.text) {
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done: false
+                };
+
+            } else {
+                alert("Tarefa obrigatória")
+            }
         }
     }
 };
